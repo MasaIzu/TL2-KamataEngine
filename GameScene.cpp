142cc660881ab6485c4af500fcb760d6b8625021
masato izumida <Oioi_0126@outlook.com>
2023-12-11 14:36:39 +0900

    プロジェクト ファイルを追加します。

---
+++ GameScene.cpp
@@ -0,0 +1,136 @@
+﻿#include "GameScene.h"
+#include "Model.h"
+#include <cassert>
+#include <sstream>
+#include <iomanip>
+
+using namespace DirectX;
+
+GameScene::GameScene()
+{
+}
+
+GameScene::~GameScene()
+{
+	delete sprite;
+	delete objSkydome;
+	delete objGround;
+	delete objFighter;
+	delete modelSkydome;
+	delete modelGround;
+	delete modelFighter;
+	delete camera;
+}
+
+void GameScene::Initialize(DirectXCommon* dxCommon, Input* input)
+{
+	// nullptrチェック
+	assert(dxCommon);
+	assert(input);
+
+	this->dxCommon = dxCommon;
+	this->input = input;
+
+	// デバッグテキスト用テクスチャ読み込み
+	Sprite::LoadTexture(debugTextTexNumber, L"Resources/beel_idle.dds");
+	// デバッグテキスト初期化
+	debugText.Initialize(debugTextTexNumber);
+
+	// テクスチャ読み込み
+	Sprite::LoadTexture(1, L"Resources/beel_idle.dds");
+
+    // カメラ生成
+	camera = new DebugCamera(WinApp::kWindowWidth, WinApp::kWindowHeight, input);
+
+	// カメラ注視点をセット
+	camera->SetTarget({0, 1, 0});
+	camera->SetDistance(3.0f);
+
+    // 3Dオブジェクトにカメラをセット
+	Object3d::SetCamera(camera);
+
+	// 背景スプライト生成
+	/*spriteBG = Sprite::Create(1, { 0.0f,0.0f });*/
+	// 3Dオブジェクト生成
+	objSkydome = Object3d::Create();
+	objGround = Object3d::Create();
+	objFighter = Object3d::Create();
+
+	// テクスチャ1番に読み込み
+	sprite = Sprite::Create(1, { 0.0f,0.0f });
+
+	modelSkydome = Model::CreateFromOBJ("skydome");
+	modelGround = Model::CreateFromOBJ("ground");
+	modelFighter = Model::CreateFromOBJ("chr_sword");
+
+	objSkydome->SetModel(modelSkydome);
+	objGround->SetModel(modelGround);
+	objFighter->SetModel(modelFighter);
+}
+
+void GameScene::Update()
+{
+	camera->Update();
+
+	objSkydome->Update();
+	objGround->Update();
+	objFighter->Update();
+}
+
+void GameScene::Draw()
+{
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* cmdList = dxCommon->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(cmdList);
+	// 背景スプライト描画
+	sprite->Draw();
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Object3d::PreDraw(cmdList);
+
+	// 3Dオブクジェクトの描画
+	/*objSkydome->Draw();
+	objGround->Draw();*/
+	/*objFighter->Draw();*/
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+
+	// 3Dオブジェクト描画後処理
+	Object3d::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(cmdList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	//// 描画
+	//sprite1->Draw();
+	//sprite2->Draw();
+
+	// デバッグテキストの描画
+	debugText.DrawAll(cmdList);
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+#pragma endregion
+}
